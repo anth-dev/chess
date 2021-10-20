ff62776fd43f417c479abe8dba9c3d8233b3685e
Anthony Giles <omgliekpie@gmail.com>
2021-10-20 13:09:20 -0600

    Create a very basic chessboard.

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
+require 'pry'
 
 class Board
+  attr_reader :board
 
-
+  def initialize(board = Array.new(8) { Array.new(8, nil) } )
+    @board = board
+  end
 end
+
+binding.pry
+puts 'test'
