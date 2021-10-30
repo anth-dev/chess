ac5f9aa9baf4179a330e63f1083fc706f8519ce4
Anthony Giles <omgliekpie@gmail.com>
2021-10-29 18:40:00 -0600

    Change the default board hash to be a multi-line declaration.

--- lib/board.rb
+++ lib/board.rb
@@ -3,7 +3,87 @@
 class Board
   attr_reader :board
 
-  def initialize(board = { :a => { :"1" => { :bg => 'dark', :here => nil }, :"2" => { :bg => 'light', :here => nil }, :"3" => { :bg => 'dark', :here => nil }, :"4" => { :bg => 'light', :here => nil }, :"5" => { :bg => 'dark', :here => nil }, :"6" => { :bg => 'light', :here => nil }, :"7" => { :bg => 'dark', :here => nil }, :"8" => { :bg => 'light', :here => nil } }, :b => { :"1" => { :bg => 'light', :here => nil }, :"2" => { :bg => 'dark', :here => nil }, :"3" => { :bg => 'light', :here => nil }, :"4" => { :bg => 'dark', :here => nil }, :"5" => { :bg => 'light', :here => nil }, :"6" => { :bg => 'dark', :here => nil }, :"7" => { :bg => 'light', :here => nil }, :"8" => { :bg => 'dark', :here => nil } }, :c => { :"1" => { :bg => 'dark', :here => nil }, :"2" => { :bg => 'light', :here => nil }, :"3" => { :bg => 'dark', :here => nil }, :"4" => { :bg => 'light', :here => nil }, :"5" => { :bg => 'dark', :here => nil }, :"6" => { :bg => 'light', :here => nil }, :"7" => { :bg => 'dark', :here => nil }, :"8" => { :bg => 'light', :here => nil } }, :d => { :"1" => { :bg => 'light', :here => nil }, :"2" => { :bg => 'dark', :here => nil }, :"3" => { :bg => 'light', :here => nil }, :"4" => { :bg => 'dark', :here => nil }, :"5" => { :bg => 'light', :here => nil }, :"6" => { :bg => 'dark', :here => nil }, :"7" => { :bg => 'light', :here => nil }, :"8" => { :bg => 'dark', :here => nil } }, :e => { :"1" => { :bg => 'dark', :here => nil }, :"2" => { :bg => 'light', :here => nil }, :"3" => { :bg => 'dark', :here => nil }, :"4" => { :bg => 'light', :here => nil }, :"5" => { :bg => 'dark', :here => nil }, :"6" => { :bg => 'light', :here => nil }, :"7" => { :bg => 'dark', :here => nil }, :"8" => { :bg => 'light', :here => nil } }, :f => { :"1" => { :bg => 'light', :here => nil }, :"2" => { :bg => 'dark', :here => nil }, :"3" => { :bg => 'light', :here => nil }, :"4" => { :bg => 'dark', :here => nil }, :"5" => { :bg => 'light', :here => nil }, :"6" => { :bg => 'dark', :here => nil }, :"7" => { :bg => 'light', :here => nil }, :"8" => { :bg => 'dark', :here => nil } }, :g => { :"1" => { :bg => 'dark', :here => nil }, :"2" => { :bg => 'light', :here => nil }, :"3" => { :bg => 'dark', :here => nil }, :"4" => { :bg => 'light', :here => nil }, :"5" => { :bg => 'dark', :here => nil }, :"6" => { :bg => 'light', :here => nil }, :"7" => { :bg => 'dark', :here => nil }, :"8" => { :bg => 'light', :here => nil } }, :h => { :"1" => { :bg => 'light', :here => nil }, :"2" => { :bg => 'dark', :here => nil }, :"3" => { :bg => 'light', :here => nil }, :"4" => { :bg => 'dark', :here => nil }, :"5" => { :bg => 'light', :here => nil }, :"6" => { :bg => 'dark', :here => nil }, :"7" => { :bg => 'light', :here => nil }, :"8" => { :bg => 'dark', :here => nil } } } )
+  def initialize(board = {
+    :a => {
+      :"1" => { :bg => 'dark', :here => nil },
+      :"2" => { :bg => 'light', :here => nil },
+      :"3" => { :bg => 'dark', :here => nil },
+      :"4" => { :bg => 'light', :here => nil },
+      :"5" => { :bg => 'dark', :here => nil },
+      :"6" => { :bg => 'light', :here => nil },
+      :"7" => { :bg => 'dark', :here => nil },
+      :"8" => { :bg => 'light', :here => nil }
+      },
+    :b => {
+      :"1" => { :bg => 'light', :here => nil },
+      :"2" => { :bg => 'dark', :here => nil },
+      :"3" => { :bg => 'light', :here => nil },
+      :"4" => { :bg => 'dark', :here => nil },
+      :"5" => { :bg => 'light', :here => nil },
+      :"6" => { :bg => 'dark', :here => nil },
+      :"7" => { :bg => 'light', :here => nil },
+      :"8" => { :bg => 'dark', :here => nil }
+      },
+    :c => {
+      :"1" => { :bg => 'dark', :here => nil },
+      :"2" => { :bg => 'light', :here => nil },
+      :"3" => { :bg => 'dark', :here => nil },
+      :"4" => { :bg => 'light', :here => nil },
+      :"5" => { :bg => 'dark', :here => nil },
+      :"6" => { :bg => 'light', :here => nil },
+      :"7" => { :bg => 'dark', :here => nil },
+      :"8" => { :bg => 'light', :here => nil }
+      },
+    :d => {
+      :"1" => { :bg => 'light', :here => nil },
+      :"2" => { :bg => 'dark', :here => nil },
+      :"3" => { :bg => 'light', :here => nil },
+      :"4" => { :bg => 'dark', :here => nil },
+      :"5" => { :bg => 'light', :here => nil },
+      :"6" => { :bg => 'dark', :here => nil },
+      :"7" => { :bg => 'light', :here => nil },
+      :"8" => { :bg => 'dark', :here => nil }
+      },
+    :e => {
+      :"1" => { :bg => 'dark', :here => nil },
+      :"2" => { :bg => 'light', :here => nil },
+      :"3" => { :bg => 'dark', :here => nil },
+      :"4" => { :bg => 'light', :here => nil },
+      :"5" => { :bg => 'dark', :here => nil },
+      :"6" => { :bg => 'light', :here => nil },
+      :"7" => { :bg => 'dark', :here => nil },
+      :"8" => { :bg => 'light', :here => nil }
+      },
+    :f => {
+      :"1" => { :bg => 'light', :here => nil },
+      :"2" => { :bg => 'dark', :here => nil },
+      :"3" => { :bg => 'light', :here => nil },
+      :"4" => { :bg => 'dark', :here => nil },
+      :"5" => { :bg => 'light', :here => nil },
+      :"6" => { :bg => 'dark', :here => nil },
+      :"7" => { :bg => 'light', :here => nil },
+      :"8" => { :bg => 'dark', :here => nil }
+      },
+    :g => {
+      :"1" => { :bg => 'dark', :here => nil },
+      :"2" => { :bg => 'light', :here => nil },
+      :"3" => { :bg => 'dark', :here => nil },
+      :"4" => { :bg => 'light', :here => nil },
+      :"5" => { :bg => 'dark', :here => nil },
+      :"6" => { :bg => 'light', :here => nil },
+      :"7" => { :bg => 'dark', :here => nil },
+      :"8" => { :bg => 'light', :here => nil }
+      },
+    :h => {
+      :"1" => { :bg => 'light', :here => nil },
+      :"2" => { :bg => 'dark', :here => nil },
+      :"3" => { :bg => 'light', :here => nil },
+      :"4" => { :bg => 'dark', :here => nil },
+      :"5" => { :bg => 'light', :here => nil },
+      :"6" => { :bg => 'dark', :here => nil },
+      :"7" => { :bg => 'light', :here => nil },
+      :"8" => { :bg => 'dark', :here => nil }
+      }})
     @board = board
   end
 
