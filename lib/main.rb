feabdf3fc5a820973e6d401bc03ba83af4d15be7
Anthony Giles <omgliekpie@gmail.com>
2021-11-02 13:07:46 -0600

    Add class for pieces.

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../lib/board'
+require_relative '../lib/piece'
 
 test_board = Board.new
 test_board.display_board
---
+++ lib/piece.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Piece
+
+  def initialize(location, owner)
+    @location = location
+    @owner = owner
+  end
+end
