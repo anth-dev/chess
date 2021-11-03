939708fc30698b7be0a728e480f4b74d9381f1ec
Anthony Giles <omgliekpie@gmail.com>
2021-11-03 15:22:25 -0600

    Change board to be nested arrays ending with a hash for each tile.

--- lib/board.rb
+++ lib/board.rb
@@ -3,87 +3,88 @@
 class Board
   attr_reader :board
 
-  def initialize(board = {
-    :a => {
-      :"1" => { :bg => 'dark', :here => nil },
-      :"2" => { :bg => 'light', :here => nil },
-      :"3" => { :bg => 'dark', :here => nil },
-      :"4" => { :bg => 'light', :here => nil },
-      :"5" => { :bg => 'dark', :here => nil },
-      :"6" => { :bg => 'light', :here => nil },
-      :"7" => { :bg => 'dark', :here => nil },
-      :"8" => { :bg => 'light', :here => nil }
-      },
-    :b => {
-      :"1" => { :bg => 'light', :here => nil },
-      :"2" => { :bg => 'dark', :here => nil },
-      :"3" => { :bg => 'light', :here => nil },
-      :"4" => { :bg => 'dark', :here => nil },
-      :"5" => { :bg => 'light', :here => nil },
-      :"6" => { :bg => 'dark', :here => nil },
-      :"7" => { :bg => 'light', :here => nil },
-      :"8" => { :bg => 'dark', :here => nil }
-      },
-    :c => {
-      :"1" => { :bg => 'dark', :here => nil },
-      :"2" => { :bg => 'light', :here => nil },
-      :"3" => { :bg => 'dark', :here => nil },
-      :"4" => { :bg => 'light', :here => nil },
-      :"5" => { :bg => 'dark', :here => nil },
-      :"6" => { :bg => 'light', :here => nil },
-      :"7" => { :bg => 'dark', :here => nil },
-      :"8" => { :bg => 'light', :here => nil }
-      },
-    :d => {
-      :"1" => { :bg => 'light', :here => nil },
-      :"2" => { :bg => 'dark', :here => nil },
-      :"3" => { :bg => 'light', :here => nil },
-      :"4" => { :bg => 'dark', :here => nil },
-      :"5" => { :bg => 'light', :here => nil },
-      :"6" => { :bg => 'dark', :here => nil },
-      :"7" => { :bg => 'light', :here => nil },
-      :"8" => { :bg => 'dark', :here => nil }
-      },
-    :e => {
-      :"1" => { :bg => 'dark', :here => nil },
-      :"2" => { :bg => 'light', :here => nil },
-      :"3" => { :bg => 'dark', :here => nil },
-      :"4" => { :bg => 'light', :here => nil },
-      :"5" => { :bg => 'dark', :here => nil },
-      :"6" => { :bg => 'light', :here => nil },
-      :"7" => { :bg => 'dark', :here => nil },
-      :"8" => { :bg => 'light', :here => nil }
-      },
-    :f => {
-      :"1" => { :bg => 'light', :here => nil },
-      :"2" => { :bg => 'dark', :here => nil },
-      :"3" => { :bg => 'light', :here => nil },
-      :"4" => { :bg => 'dark', :here => nil },
-      :"5" => { :bg => 'light', :here => nil },
-      :"6" => { :bg => 'dark', :here => nil },
-      :"7" => { :bg => 'light', :here => nil },
-      :"8" => { :bg => 'dark', :here => nil }
-      },
-    :g => {
-      :"1" => { :bg => 'dark', :here => nil },
-      :"2" => { :bg => 'light', :here => nil },
-      :"3" => { :bg => 'dark', :here => nil },
-      :"4" => { :bg => 'light', :here => nil },
-      :"5" => { :bg => 'dark', :here => nil },
-      :"6" => { :bg => 'light', :here => nil },
-      :"7" => { :bg => 'dark', :here => nil },
-      :"8" => { :bg => 'light', :here => nil }
-      },
-    :h => {
-      :"1" => { :bg => 'light', :here => nil },
-      :"2" => { :bg => 'dark', :here => nil },
-      :"3" => { :bg => 'light', :here => nil },
-      :"4" => { :bg => 'dark', :here => nil },
-      :"5" => { :bg => 'light', :here => nil },
-      :"6" => { :bg => 'dark', :here => nil },
-      :"7" => { :bg => 'light', :here => nil },
-      :"8" => { :bg => 'dark', :here => nil }
-      }})
+  def initialize(board = [
+    [
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil }
+    ],
+    [
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil }
+    ],
+    [
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil }
+    ],
+    [
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil }
+    ],
+    [
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil }
+    ],
+    [
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil }
+    ],
+    [
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil }
+    ],
+    [
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil },
+      { bg: 'light', here: nil },
+      { bg: 'dark', here: nil }
+    ]])
+
     @board = board
   end
 
@@ -92,21 +93,22 @@ class Board
 
     puts %(
        A B C D E F G H
-      8#{display_cell('a8')}#{display_cell('b8')}#{display_cell('c8')}#{display_cell('d8')}#{display_cell('e8')}#{display_cell('f8')}#{display_cell('g8')}#{display_cell('h8')}
-      7#{display_cell('a7')}#{display_cell('b7')}#{display_cell('c7')}#{display_cell('d7')}#{display_cell('e7')}#{display_cell('f7')}#{display_cell('g7')}#{display_cell('h7')}
-      6#{display_cell('a6')}#{display_cell('b6')}#{display_cell('c6')}#{display_cell('d6')}#{display_cell('e6')}#{display_cell('f6')}#{display_cell('g6')}#{display_cell('h6')}
-      5#{display_cell('a5')}#{display_cell('b5')}#{display_cell('c5')}#{display_cell('d5')}#{display_cell('e5')}#{display_cell('f5')}#{display_cell('g5')}#{display_cell('h5')}
-      4#{display_cell('a4')}#{display_cell('b4')}#{display_cell('c4')}#{display_cell('d4')}#{display_cell('e4')}#{display_cell('f4')}#{display_cell('g4')}#{display_cell('h4')}
-      3#{display_cell('a3')}#{display_cell('b3')}#{display_cell('c3')}#{display_cell('d3')}#{display_cell('e3')}#{display_cell('f3')}#{display_cell('g3')}#{display_cell('h3')}
-      2#{display_cell('a2')}#{display_cell('b2')}#{display_cell('c2')}#{display_cell('d2')}#{display_cell('e2')}#{display_cell('f2')}#{display_cell('g2')}#{display_cell('h2')}
-      1#{display_cell('a1')}#{display_cell('b1')}#{display_cell('c1')}#{display_cell('d1')}#{display_cell('e1')}#{display_cell('f1')}#{display_cell('g1')}#{display_cell('h1')}
+      8#{display_cell('07')}#{display_cell('17')}#{display_cell('27')}#{display_cell('37')}#{display_cell('47')}#{display_cell('57')}#{display_cell('67')}#{display_cell('77')}
+      7#{display_cell('06')}#{display_cell('16')}#{display_cell('26')}#{display_cell('36')}#{display_cell('46')}#{display_cell('56')}#{display_cell('66')}#{display_cell('76')}
+      6#{display_cell('05')}#{display_cell('15')}#{display_cell('25')}#{display_cell('35')}#{display_cell('45')}#{display_cell('55')}#{display_cell('65')}#{display_cell('75')}
+      5#{display_cell('04')}#{display_cell('14')}#{display_cell('24')}#{display_cell('34')}#{display_cell('44')}#{display_cell('54')}#{display_cell('64')}#{display_cell('74')}
+      4#{display_cell('03')}#{display_cell('13')}#{display_cell('23')}#{display_cell('33')}#{display_cell('43')}#{display_cell('53')}#{display_cell('63')}#{display_cell('73')}
+      3#{display_cell('02')}#{display_cell('12')}#{display_cell('22')}#{display_cell('32')}#{display_cell('42')}#{display_cell('52')}#{display_cell('62')}#{display_cell('72')}
+      2#{display_cell('01')}#{display_cell('11')}#{display_cell('21')}#{display_cell('31')}#{display_cell('41')}#{display_cell('51')}#{display_cell('61')}#{display_cell('71')}
+      1#{display_cell('00')}#{display_cell('10')}#{display_cell('20')}#{display_cell('30')}#{display_cell('40')}#{display_cell('50')}#{display_cell('60')}#{display_cell('70')}
        A B C D E F G H
     )
   end
 
   def display_cell(location)
-    x = location[0].to_sym
-    y = location[1].to_sym
+    x = location[0].to_i
+    y = location[1].to_i
+  
     display_value = (@board[x][y][:here].determine_display_value << ' ') if @board[x][y][:here]
 
     background_color = board[x][y][:bg]
