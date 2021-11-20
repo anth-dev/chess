dbf33232f313a3c013c001447e5fa90cb0e62b96
Anthony Giles <omgliekpie@gmail.com>
2021-11-19 19:43:27 -0700

    Create method for finding adjacent moves

--- lib/knight.rb
+++ lib/knight.rb
@@ -13,4 +13,24 @@ class Knight < Piece
       "\u265E".encode('utf-8')
     end
   end
+
+  def find_adjacent_moves
+    adjacent_moves = []
+
+    x = @location[0].to_i
+    y = @location[1].to_i
+
+    adjacent_moves << [x - 1, y - 2]
+    adjacent_moves << [x - 2, y - 1]
+    adjacent_moves << [x - 2, y + 1]
+    adjacent_moves << [x - 1, y + 2]
+    adjacent_moves << [x + 1, y + 2]
+    adjacent_moves << [x + 2, y + 1]
+    adjacent_moves << [x + 2, y - 1]
+    adjacent_moves << [x + 1, y - 2]
+
+    adjacent_moves.filter do |move|
+      (move[0] >= 0 && move[0] <= 7) && (move[1] >= 0 && move[1] <= 7)
+    end
+  end
 end
